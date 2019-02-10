4bef7ac8af1414d7c71d15fdc30b919a54bd1ffc
BolajiOlajide <bolaji.olajide@andela.com>
2019-02-10 12:49:18 +0100

    learn about sed

---
+++ Developer Tools/sed/sed.md
@@ -0,0 +1,5 @@
+# SED
+
+SED - stands for stream editor.
+
+It is used to perform basic text transformations on an input stream ( a file or input from a pipeline).
---
+++ Developer Tools/sed/sed.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# sed stands for string editor
+cat text | sed 's/t/T/' >> output # this replaces every t found in the text file to T and saves the result to a file called output
+
+# Yoy can pass a file to the sed command w/o piping into it
+sed 's/t/T/' text # the s stands for substitute but doesn't modify the line
+# this command only does search and replace for the first instance on each line
+
+# to substitute all lower case t's and transform them you use the /g to select global t's
+sed 's/t/T/g' text
+
+# to modify the original file use the -i flag
+sed -i 's/t/T/g' text
